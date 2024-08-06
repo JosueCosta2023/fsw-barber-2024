d1d79089fb9f9992d2e3dcc3f93e3e8a8c077845
Josue Costa <contato_josuecosta@hotmail.com>
2024-08-06 15:58:08 -0400

    feat: add booking components

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,11 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
-import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
 import Footer from "./_components/footer"
 import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-Item"
 
 const Home = async () => {
   const barbershop = await db.barbershop.findMany({})
@@ -51,7 +49,7 @@ const Home = async () => {
         </div>
 
         {/* Banner */}
-        <div className="relative mt-6 h-[150px] w-full">
+        <div className="relative mt-6 h-[150px] w-full max-w-[468px]">
           <Image
             alt="Agende no melhores com a FSW Barber"
             src="/banner-01.png"
@@ -61,31 +59,7 @@ const Home = async () => {
         </div>
 
         {/* Agendamento */}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/** Left */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/0522fdaf-0357-4213-8f52-1d83c3dcb6cd-18e.png" />
-                </Avatar>
-                <p className="text-sm">Vintage Barber</p>
-              </div>
-            </div>
-
-            {/**Right */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">21:30</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         {/** Recomendados  */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
