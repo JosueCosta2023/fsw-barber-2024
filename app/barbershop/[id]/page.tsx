d292528bbc22e83cdce73162d52ac0e19275b6c5
Josue Costa <contato_josuecosta@hotmail.com>
2024-08-07 16:02:41 -0400

    feat: ensure arbershopservice are shown on barbershoppage

--- app/barbershop/[id]/page.tsx
+++ app/barbershop/[id]/page.tsx
@@ -1,8 +1,10 @@
+import ServiceItem from "@/app/_components/service-item"
 import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { notFound } from "next/navigation"
 
 interface BarbershopPageProps {
   params: {
@@ -15,7 +17,16 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
     where: {
       id: params.id,
     },
+    include: {
+      services: true,
+    },
   })
+
+  if (!barbershop) {
+    return notFound()
+  }
+
+  console.log(barbershop.services)
 
   return (
     <div>
@@ -64,6 +75,18 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       <div className="space-y-3 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop?.description}</p>
+      </div>
+
+      {/**Servios */}
+      <div className="p-5">
+        <h2 className="mb-3 text-xs font-bold uppercase text-gray-400">
+          Serviços
+        </h2>
+        <div className="min-w-[320px] max-w-[768px] space-y-3">
+          {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
-import Footer from "./_components/footer"
 import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-Item"
 
@@ -81,8 +80,6 @@ const Home = async () => {
           ))}
         </div>
       </div>
-
-      <Footer />
     </div>
   )
 }
